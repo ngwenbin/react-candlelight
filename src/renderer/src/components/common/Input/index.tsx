4e55ef348f33f4ca5850a247762dc964fc687fc7
Wen Bin <nwb@super-powerful-fruit.local>
2024-04-10 20:07:11 +0800

    chore: added forward ref to forminput

--- src/renderer/src/components/common/Input/index.tsx
+++ src/renderer/src/components/common/Input/index.tsx
@@ -1,5 +1,5 @@
 import clsx from "clsx"
-import { InputHTMLAttributes } from "react"
+import { InputHTMLAttributes, forwardRef } from "react"
 
 interface FormInputProps
   extends Omit<InputHTMLAttributes<HTMLInputElement>, "id"> {
@@ -12,28 +12,36 @@ interface FormInputProps
 /**
  * Uncontrolled Input component with label
  */
-export const FormInput = ({
-  id,
-  label,
-  className,
-  containerClassName,
-  errors,
-  ...props
-}: FormInputProps) => {
-  return (
-    <div className={clsx("flex flex-col gap-y-2", containerClassName)}>
-      <label htmlFor={id} className="font-medium">
-        {label}
-      </label>
-      <input
-        {...props}
-        id={id}
-        className={clsx(
-          "placeholder:text-gray-5 w-full text-primary-light bg-primary-dark border-gray-7 min-h-[32px] border rounded text-base px-3 focus-visible:outline-gray-1 focus-visible:outline-1 focus-visible:outline focus-visible:outline-offset-0",
-          className
-        )}
-      />
-      <div className="text-xs text-red-400">{errors}</div>
-    </div>
-  )
-}
+export const FormInput = forwardRef(
+  (
+    {
+      id,
+      label,
+      className,
+      containerClassName,
+      errors,
+      ...props
+    }: FormInputProps,
+    ref: React.ForwardedRef<HTMLInputElement>
+  ) => {
+    return (
+      <div className={clsx("flex flex-col gap-y-2", containerClassName)}>
+        <label htmlFor={id} className="font-medium text-primary-light">
+          {label}
+        </label>
+        <input
+          ref={ref}
+          {...props}
+          id={id}
+          className={clsx(
+            "placeholder:text-gray-5 w-full text-primary-light bg-primary-dark border-gray-7 min-h-[32px] border rounded text-base px-3 focus-visible:outline-gray-1 focus-visible:outline-1 focus-visible:outline focus-visible:outline-offset-0",
+            className
+          )}
+        />
+        <div className="text-xs text-red-400">{errors}</div>
+      </div>
+    )
+  }
+)
+
+FormInput.displayName = "FormInput"
