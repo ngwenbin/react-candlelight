b8ce610726e104aabad37b7f280cc22fd8f3b7ae
Wen Bin <nwb@super-powerful-fruit.local>
2024-04-10 20:10:08 +0800

    chore: remove unnecessary div in modal

--- src/renderer/src/components/common/modal/index.tsx
+++ src/renderer/src/components/common/modal/index.tsx
@@ -9,21 +9,19 @@ interface ModalProps {
 
 export const Modal = ({ title, onClose, children }: ModalProps) => {
   return (
-    <div>
-      <div className="group absolute w-full min-h-screen max-h-screen top-0 left-0 backdrop-blur-md bg-primary-dark/90 flex flex-col py-8 px-6">
-        <div className="font-bold pb-6 text-primary-light flex justify-between items-center">
-          {title}
-          <Button
-            icon={
-              <RiCloseLine className="text-primary-light hover:text-primary-main" />
-            }
-            variant="actionLink"
-            className="pt-0.5"
-            onClick={onClose}
-          />
-        </div>
-        {children}
+    <div className="group absolute w-full min-h-screen max-h-screen top-0 left-0 backdrop-blur-md bg-primary-dark/90 flex flex-col py-8 px-6">
+      <div className="font-bold pb-6 text-primary-light flex justify-between items-center">
+        <div>{title}</div>
+        <Button
+          icon={
+            <RiCloseLine className="text-primary-light hover:text-primary-main" />
+          }
+          variant="actionLink"
+          className="pt-0.5"
+          onClick={onClose}
+        />
       </div>
+      {children}
     </div>
   )
 }
